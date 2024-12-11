aa3d7bc77ac96bc455c25cbbb87b7542956e6ced
Hannah Gilliam-Vigh <hannahgvs@gmail.com>
2024-12-11 09:58:44 +0100

    add plot_distributions to functions

--- R/functions.R
+++ R/functions.R
@@ -4,9 +4,21 @@
 #'
 #' @return A data.frame/tibble.
 
-descriptive_stats <- function(data){
-    data %>%
-        dplyr::group_by(metabolite) %>%
-        dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
-        dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
+descriptive_stats <- function(data) {
+  data %>%
+    dplyr::group_by(metabolite) %>%
+    dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
+    dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
+}
+
+#' Title Plot distributions
+#'
+#' @param data Lipidomics data
+#'
+#' @return plots the distribution of values one histogram per metabolite
+
+plot_distributions <- function(data) {
+  ggplot2::ggplot(data, ggplot2::aes(x = value)) +
+    ggplot2::geom_histogram() +
+    ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
 }
