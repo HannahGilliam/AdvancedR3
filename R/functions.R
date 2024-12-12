5a6dad1cd04a06993ec88e147445e3bb4e919e36
Hannah Gilliam-Vigh <hannahgvs@gmail.com>
2024-12-12 11:02:06 +0100

    add targets: calculate estimates

--- R/functions.R
+++ R/functions.R
@@ -119,12 +119,33 @@ split_by_metabolite <- function(data) {
 #' @return A data frame.
 #'
 generate_model_results <- function(data) {
-    create_model_workflow(
-        parsnip::logistic_reg() |>
-            parsnip::set_engine("glm"),
-        data |>
-            create_recipe_spec(tidyselect::starts_with("metabolite_"))
-    ) |>
-        parsnip::fit(data) |>
-        tidy_model_output()
+  create_model_workflow(
+    parsnip::logistic_reg() |>
+      parsnip::set_engine("glm"),
+    data |>
+      create_recipe_spec(tidyselect::starts_with("metabolite_"))
+  ) |>
+    parsnip::fit(data) |>
+    tidy_model_output()
+}
+
+#' Calculate the estimates for the model for each metabolite.
+#'
+#' @param data The lipidomics dataset.
+#'
+#' @return A data frame.
+#'
+calculate_estimates <- function(data) {
+  model_estimates <- data |>
+    split_by_metabolite() |>
+    purrr::map(generate_model_results) |>
+    purrr::list_rbind() |>
+    dplyr::filter(stringr::str_detect(term, "metabolite_"))
+  data %>%
+    dplyr::select(metabolite) %>%
+    dplyr::mutate(term = metabolite) %>%
+    column_values_to_snake_case(term) %>%
+    dplyr::mutate(term = stringr::str_c("metabolite_", term)) %>%
+    dplyr::distinct(term, metabolite) %>%
+    dplyr::right_join(model_estimates, by = "term")
 }
--- _targets.R
+++ _targets.R
@@ -69,5 +69,9 @@ list(
   tar_quarto(
     name = quarto_doc,
     path = "doc/learning.qmd"
+  ),
+  tar_target(
+    name = df_model_estimates,
+    command = calculate_estimates(lipidomics)
   )
 )
